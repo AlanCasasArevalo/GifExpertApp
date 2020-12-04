a0d35201a035475ee2ac67f731f8cd8018b246f3
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-04 19:14:52 +0100

    [FEATURE][GIF EXPERT] * New button was added to add new categories

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -3,10 +3,28 @@ import PropTypes from 'prop-types'
 
 const GifExpertApp = () => {
 
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+
+    const handleAdd = () => {
+        console.log(`ssss`)
+        setCategories([...categories, 'Dragon Ball Z'])
+    }
+
     return (
         <>
             <h1>GifExpertApp</h1>
             <hr/>
+
+            <button onClick={ handleAdd }>Agregar un nueva serie</button>
+
+            <ol>
+                {
+                    categories.map( (category, index) => {
+                        return <li key={category}> { category } </li>
+                    })
+                }
+            </ol>
+
         </>
     )
 }
