a325c406036aa4213794063be9a55db3c593272d
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-04 18:34:36 +0100

    [CONFIG][PROJECT INIT] * First setup to learn more about React

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const GifExpertApp = () => {
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+            <hr/>
+        </>
+    )
+}
+//
+// GifExpertApp.propTypes = {
+//     value: PropTypes.number
+// }
+
+export default GifExpertApp;
+
