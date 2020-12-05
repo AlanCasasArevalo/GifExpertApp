ab2009a28d0d15711ad22a3cc5f11a728424ba7a
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-05 11:38:21 +0100

    [TEST][GRID ITEM] * Property props test was added * Image tests was added * Div class test was added

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const GridItem = ({id, title, url}) => {
+const GridItem = ({ title, url}) => {
 
     // Animation came from https://animate.style/
     return (
         <div className="card animate__animated animate__flash">
-            <img src={url} alt={title}
+            <img
+                src={url}
+                alt={title}
             />
             <p>{title}</p>
         </div>
     )
+}
+
+
+GridItem.propTypes = {
+    title: PropTypes.string,
+    url: PropTypes.string
 }
 
 export default GridItem;
--- src/test/components/GridItem.test.js
+++ src/test/components/GridItem.test.js
@@ -4,10 +4,21 @@ import GridItem from "../../components/GridItem";
 
 describe('GridItem Component test', () => {
 
-    let wrapper = shallow(<GridItem />)
+    const title = 'Hola, soy Alan'
+    const url = 'http://localhost:3000/image'
+    let wrapper = shallow(
+        <GridItem
+            title={title}
+            url={url}
+        />)
+
 
     beforeEach(() => {
-        wrapper = shallow(<GridItem />)
+        wrapper = shallow(
+            <GridItem
+                title={title}
+                url={url}
+            />)
     })
 
     test('GridItem should be defined', () => {
@@ -19,5 +30,23 @@ describe('GridItem Component test', () => {
 
     test('GridItem should return a snapshot', () => {
         expect(wrapper).toMatchSnapshot()
+    })
+
+    test('GridItem should return title by props', () => {
+        const titleInP = wrapper.find('p').text(title)
+        expect(title).toBe(titleInP)
+    })
+
+    test('GridItem should return url and title into image by props', () => {
+        const image = wrapper.find('img')
+        const { src, alt } = image.props()
+        expect(url).toStrictEqual(src)
+        expect(title).toStrictEqual(alt)
+    })
+
+    test('GridItem should get animate__flash class into div', () => {
+        const div = wrapper.find('div')
+        const {className, children} = div.props()
+        expect(className).toStrictEqual('card animate__animated animate__flash')
     })
 })
